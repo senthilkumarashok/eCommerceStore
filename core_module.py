005a9b6a420714d74153676e621ef9e74d74e599
senkumar <senthil.java.2015@gmail.com>
2024-03-03 11:33:09 -0600

    Update core_module.py

--- core_module.py
+++ core_module.py
@@ -1,8 +1,8 @@
-from itertools import count
+import itertools
 
 class Category:
     
-    category_id_counter = count(start=1) # counter to generate sequential Ids for categoryId
+    category_id_counter = itertools.count(start=1) # counter to generate sequential Ids for categoryId
     
     def __init__(self, category_name):
         self.category_id = next(self.category_id_counter);
@@ -10,7 +10,7 @@ class Category:
 
 class Product:
     
-    product_id_counter = count(start=1) # counter to generate sequential Ids for productId
+    product_id_counter = itertools.count(start=1) # counter to generate sequential Ids for productId
     
     def __init__(self, product_name, category_id, price):        
         self.product_id = next(self.product_id_counter);
@@ -18,44 +18,30 @@ class Product:
         self.category_id  = category_id
         self.price = price
 
-class ShoppingCart:
-
-    def __init__(self):
-        self.items = []
-
-    def add_items(self, item_name, qty):
-        item = (item_name, qty)
-        self.items.append(item)
-        print("Added to cart successfully")
-
-    def list_items(self):
-        format_row = "{:<10}  {:<10}  {:<10}"       
-        print(format_row.format( "ItemNo", "ItemName", "Qty"))
-        print(format_row.format( "==========", "==========", "=========="))
-        for item in self.items:        
-            print(format_row.format( 0, item[0], item[1]))    
-
-    def remove_item(self, item_name):
-        for item in self.items:
-            if item[0] == item_name:
-                self.items.remove(item)
-                break
-
-    def list_cart_price(self):
-        sum = 0
-        for item in self.items:
-            sum = sum + item.price
-        return sum           
-
 class Store:
         
     def __init__(self):
         self.categories = [] # Datasource for storing categories
         self.products = [] # Datasource for storing products
     
-    def add_category(self, category_name):        
+    def add_category(self, category_name):    
         self.categories.append(Category(category_name))
         # print(f"Category name '{category_name}' successfully added")
+
+    def del_category(self, category_name):
+        category_to_remove = None
+        for category in self.categories:
+            if(category_name == category.category_name):
+                category_to_remove = category.category_id
+                self.categories.remove(category)
+                break
+        if(category_to_remove is None):
+            print("Category not found")
+        else:
+            # print(f"Category found: {category_to_remove}")
+            for product in self.products:
+                if(category_to_remove == product.category_id):
+                    self.products.remove(product)
 
     def add_categories(self, categories):
         for category_name in categories:
@@ -70,18 +56,63 @@ class Store:
         # print(f"Product name '{product_name}' successfully added")
 
 
-    def listCategories(self):        
-        format_row = "{:<10}  {:<10}"       
-        print(format_row.format( "CategoryId", "CategoryName"))
-        print(format_row.format( "==========", "============"))
+    def listCategories(self):
+        headers = ["CategoryId", "CategoryName"];
+        format_row = "{:<10}  {:<10}"
+        headerFormat = format_row.format(headers[0], headers[1])
+
+        format_summary = "".join(itertools.repeat("-",len(headerFormat)))
+        print(format_summary)
+        print("View Categories")
+        print(format_summary)
+                
+        
+        print(headerFormat)        
+        print(format_row.format( "__________", "____________"))        
         for category in self.categories:
             header1 = "{}.".format(category.category_id)
             print(format_row.format( header1, category.category_name))
 
+        print(format_summary)
+        print()    
+
     def listProducts(self):
-        format_row = "{:<10}  {:<10}  {:<10}"       
-        print(format_row.format( "productNo", "Name", "Price"))
+        headers = ["productNo", "Name", "Price"];
+        format_row = "{:<10}  {:<10}  {:<10}" 
+        headerFormat = format_row.format(headers[0], headers[1], headers[2])      
+
+        format_summary = "".join(itertools.repeat("-",len(headerFormat)))
+        print(format_summary)
+        print("View Products")
+        print(format_summary)
+
+        print(headerFormat)
         print(format_row.format( "==========","====", "======"))
         for product in self.products:
             header1 = "{}.".format(product.product_id)
             print(format_row.format(header1,  product.product_name, product.price)) 
+
+        print(format_summary)
+        print()        
+
+# Initializing a store 
+store = Store()
+
+# Adding categories
+store.add_categories(["footwear", "clothing", "electronics"])
+
+# View categories
+store.listCategories()
+
+# Adding products
+store.add_product("shoes", "footwear", 19)
+store.add_product("shirt", "clothing", 12)
+store.add_product("camera", "electronics", 11)
+
+# View products
+store.listProducts()
+
+store.del_category("clothing")
+
+store.listCategories()
+store.listProducts()
