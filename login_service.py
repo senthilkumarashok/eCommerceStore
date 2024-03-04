c9af29ed41e7601370324a8e62992774edbab593
senkumar <senthil.java.2015@gmail.com>
2024-03-03 20:43:08 -0600

    Update login_service.py

--- login_service.py
+++ login_service.py
@@ -5,14 +5,21 @@ class LoginService:
     
     @staticmethod
     def userLogin(userName, password):
+        is_authenticated = False
         if userName in LoginService.userCreds and LoginService.userCreds[userName] == password:
             print("\nLogin successful!")
+            is_authenticated = True
         else:
-            print("\nLogin invalid")        
+            print("\nLogin invalid")
+        return is_authenticated    
+
 
     @staticmethod
     def adminLogin(userName, password):
+        is_authenticated = False
         if userName in LoginService.adminCreds and LoginService.adminCreds[userName] == password:
             print("\nLogin successful!")
+            is_authenticated = True
         else:
             print("\nLogin invalid")
+        return is_authenticated        
