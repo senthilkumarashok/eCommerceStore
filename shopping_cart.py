bf3bbdc2f3074205221fce0ead8268c04c6989c5
senkumar <senkumar@adobe.com>
2024-03-04 21:00:14 -0600

    fix lint errors

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,7 +1,10 @@
+"""Module providing a function"""
 import itertools
+import dataclasses
 
-
+@dataclasses.dataclass
 class CartItem:
+    """Class representing a CartItem"""
     def __init__(self, item_name, qty, price):
         self.item_name = item_name
         self.qty = qty
@@ -9,35 +12,37 @@ class CartItem:
 
 
 class ShoppingCart:
-
+    """Class representing a ShoppingCart"""
     def __init__(self):
         self._items = []
         self.cost = 0
 
     def add_item(self, item_name, qty, price):
+        """Function for adding items in the cart."""
         self._items.append(CartItem(item_name, qty, price))
         self.cost += qty * price
         print("Added item to cart successfully !!", len(self._items))
 
     def list_items_in_cart(self):
+        """Function for displaying items in the cart."""
         if len(self._items) == 0:
             print("cart is empty !!!")
         else:
             headers = ["ItemNo", "ItemName", "Qty"]
             format_row = "{:<8}  {:<10}  {:<10}"
-            headerFormat = format_row.format(headers[0], headers[1], headers[2])
+            header_format = format_row.format(headers[0], headers[1], headers[2])
 
-            format_summary = "".join(itertools.repeat("-", len(headerFormat)))
+            format_summary = "".join(itertools.repeat("-", len(header_format)))
 
             print(format_summary)
             print("Cart Summary")
             print(format_summary)
 
-            print(headerFormat)
+            print(header_format)
             print(format_row.format("=======", "==========", "======"))
 
             for idx, item in enumerate(self._items):
-                header1 = "{}.".format(idx + 1)
+                header1 = f"{idx + 1}."
                 print(format_row.format(header1, item.item_name, item.qty))
 
             print(format_summary)
@@ -46,6 +51,7 @@ class ShoppingCart:
             print()
 
     def remove_item(self, item_name):
+        """Function for removing items in the cart."""
         for item in self._items:
             if item.item_name == item_name:
                 print(f"Removing item {item_name}")
@@ -54,13 +60,15 @@ class ShoppingCart:
         # print(f"Removed item named '{item_name}' successfully !!!")
 
     def is_empty(self):
+        """Function to check whether cart is empty."""
         return len(self._items) > 0
 
     def checkout(self):
-        # self.list_items_in_cart()
+        """Function to checkout once cart is ready."""
         print("Your order is successfully placed")
-        print(
-            f"You will be shortly redirected to the portal for Unified Payment Interface to make a payment of Rs. {self.cost}"
+        print(f"You will be shortly redirected to the portal \
+              for Unified Payment Interface to make a \
+              payment of Rs. {self.cost}"
         )
 
 
